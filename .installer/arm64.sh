a0b7bef8cf06833730043eecd242182571e93920
Alex Breedveld <alex@al3xdev.com>
2023-04-09 16:33:31 +0000

    - Fixed installer. - Updated CI/CD.

--- .installer/arm64.sh
+++ .installer/arm64.sh
@@ -3,12 +3,13 @@
 touch /etc/apt/sources.list.d/ubuntu-toolchain-r-ubuntu-test-focal.list
 echo "deb http://ppa.launchpad.net/ubuntu-toolchain-r/test/ubuntu focal main" >> /etc/apt/sources.list.d/ubuntu-toolchain-r-ubuntu-test-focal.list
 apt-get update && apt-get install unzip jq -y && apt dist-upgrade
-unzip ./artifacts.zip
-mv ./build/ctcp /usr/bin/
+unzip /opt/ctcp/artifacts.zip -d /opt/ctcp/
+mv /opt/ctcp/build/ctcp /usr/bin/
 mkdir -p /etc/ctcp/
 touch /etc/ctcp/ctcp.json
-echo "{ \"port\": \"5520\" }" >> /etc/ctcp/ctcp.json
+echo -e "{\n \"port\": \"5520\",\n \"secs\": \"1\"\n}" >> /etc/ctcp/ctcp.json
 wget 'https://git.al3xdev.com/al3xdev/ctcp/-/raw/master/.installer/ctcp.service' -O /etc/systemd/system/ctcp.service
 systemctl daemon-reload && systemctl enable ctcp.service && systemctl start ctcp.service
+rm -r /opt/ctcp/build/
 clear
 echo -e "cTCP has been installed as a service, the default port is: 5520.\nYou may change it at: /etc/ctcp/ctcp.json and run 'systemctl restart ctcp.service'"
